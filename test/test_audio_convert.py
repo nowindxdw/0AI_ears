4efcd8797d64321b5a6ffc65c3eb2857c7bd0004
dawei <dawei@daweis-MacBook-Pro.local>
2017-11-22 19:50:07 +0800

    add split func

--- test/test_audio_convert.py
+++ test/test_audio_convert.py
@@ -4,12 +4,15 @@ import os
 
 from xears.data_utils import convert_utils
 from xears.data_utils import show_pcm
+from xears.data_utils import split_audio
 #xears_test
 
 def test_vector_classification():
     wav_name = convert_utils.convert_mp3_to_wav(os.path.dirname(__file__)+'/xears/mp3source/model.mp3')
     print(wav_name)
-    show_pcm.show(wav_name,0,5000)
+    #show_pcm.show(wav_name,0,5000)
+    train_ori = split_audio.split(wav_name )
+    print(train_ori.shape)
 
 if __name__ == '__main__':
     pytest.main([__file__])
--- test/xears/data_utils/__init__.py
+++ test/xears/data_utils/__init__.py
@@ -1,7 +1,9 @@
 from __future__ import absolute_import
 from . import convert_utils
 from . import show_pcm
+from . import split_audio
 
 # Globally-importable utils.
 from .convert_utils import convert_mp3_to_wav
 from .show_pcm import show
+from .split_audio import split
