1d4822e1a76d6c9625210d182788c355f53d5796
dawei <dawei@daweis-MacBook-Pro.local>
2017-12-12 15:07:51 +0800

    add show pcm for split

--- test/test_read_waveFile.py
+++ test/test_read_waveFile.py
@@ -7,11 +7,20 @@ from xears.data_utils import split_audio
 from xears.data_utils import show_pcm
 #xears_test
 
-def test_read_file():
+def test_read_file(type="bin"):
     output_base_path = os.path.dirname(__file__)+os.path.sep+"xears"+os.path.sep+"data_source"+os.path.sep
-    binfile = output_base_path+"model.bin"
-    output = np.fromfile(binfile,dtype='float')
-    output.shape = 33,400000,2
+
+    if type == 'bin':
+        binfile = output_base_path+"model.bin"
+        output = np.fromfile(binfile,dtype='float')
+        output.shape = 33,400000,2
+    if type == 'npy':
+        npyfile = output_base_path+"model.npy"
+        output = np.load(npyfile)
+    if type == 'file':
+        file_path = output_base_path+"modelFile.npy"
+        f = file(file_path,"rb")
+        output = np.load(f)
     show_pcm.showWavArray(output[0],0,400000)
     #print(output.shape)
     #print(type(output))
--- test/test_split_wave.py
+++ test/test_split_wave.py
@@ -8,14 +8,14 @@ from xears.data_utils import split_audio
 
 #xears_test
 
-def test_show_pcm():
+def test_split_wave():
     wav_name = os.path.dirname(__file__)+os.path.sep+"xears"+os.path.sep+"mp3source"+os.path.sep+"model.wav"
     print(wav_name)
     wav_splits = split_audio.split(wav_name,400000)
     #print(type(wav_splits))
     #print(wav_splits.shape)
     output_base_path = os.path.dirname(__file__)+os.path.sep+"xears"+os.path.sep+"data_source"+os.path.sep
-    #split_audio.store(wav_splits,output_base_path+"model.bin",store_type = "bin")
+    split_audio.store(wav_splits,output_base_path+"model.bin",store_type = "bin")
     #split_audio.store(wav_splits,output_base_path+"model.npy",store_type = "npy")
     #split_audio.store(wav_splits,output_base_path+"modelFile.npy",store_type = "file")
 
